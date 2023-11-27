6741f888880302ede6f950505f9ca1e56868fbba
Ivan Aliaga <ialiagadev@gmail.com>
2023-11-27 19:26:01 +0100

    añadir nueva clase

---
+++ src/calculadora.java
@@ -0,0 +1,9 @@
+public class calculadora {
+    public static int sumar(int a, int b) {
+        return a + b;
+    }
+
+    public static int restar(int a, int b) {
+        return a - b;
+    }
+}
